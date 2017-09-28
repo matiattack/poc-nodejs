e923bcdeeaa12968fc357bb1781b4f7f8f7bb480
Matías Aguirre <maguirre@minjusticia.cl>
2017-09-28 15:24:37 -0300

    Poc con ejemplo de TV-SHOWS

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ var express  = require("express"),
     methodOverride = require("method-override"),
     mongoose = require('mongoose');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var tvShowsControllers = require('./controllers/tvshowsController')(express);
+
+app.use('/tvshows', tvShowsControllers);
+
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+    if(err) {
+        console.log('ERROR: connecting to Database. ' + err);
+    } else {
+        server.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+    }
 });
